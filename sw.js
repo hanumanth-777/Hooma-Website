f9063e7f4ad7e145de58321e82da4dab33b209f8
hanumanth-777 <hanumanthpandari@gmail.com>
2025-08-20 18:35:04 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,4 @@
+// Simple Service Worker for Hooma
 const CACHE_NAME = 'hooma-v1';
 const urlsToCache = [
   '/Hooma-Website/',
@@ -8,9 +9,38 @@ const urlsToCache = [
   '/Hooma-Website/images/icon-512.png'
 ];
 
+// Install event - cache files immediately
 self.addEventListener('install', event => {
+  self.skipWaiting(); // Activate immediately
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event - serve from cache if available
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// Activate event - clean up old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
   );
 });
